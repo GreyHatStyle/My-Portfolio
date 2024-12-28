8672530faae52ee52143ce7d2fa9577b9ed60140
Manas Bisht <manasbisht1142004@gmail.com>
2024-12-28 14:55:45 +0530

    NAVBAR COMPLETED!    * Sticked to top and configured all elements according to it.    * Gave Shadows according to both modes.    * Added Logo (Manas)

--- JS/navbar-oc.js
+++ JS/navbar-oc.js
@@ -1,5 +1,5 @@
-const sidebar = document.getElementById("sidebar")
-
+const sidebar = document.getElementById("sidebar");
+const main_navbar = document.getElementById("navbar");
 function openMenuBar(){
     sidebar.setAttribute("class", "show");
     console.log(sidebar);
@@ -8,3 +8,12 @@ function openMenuBar(){
 function closeMenuBar(){
     sidebar.setAttribute("class", "hide");
 }
+
+document.addEventListener("scroll", ()=>{
+    if(window.scrollY > 1){
+        main_navbar.classList.add("nav-shadow");
+    }
+    else{
+        main_navbar.classList.remove("nav-shadow");
+    }
+})
